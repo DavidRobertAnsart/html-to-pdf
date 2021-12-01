d51fca7d09e495174edefb9d2a8a5f7beecc9abe
DavidRobertAnsart <david.robertansart@datadoghq.com>
2021-12-01 10:49:34 +0100

    :tada: Initialize typescript project

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+module.exports = {
+    env: {
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint', 'prettier'],
+    rules: {
+        'prettier/prettier': 2, // Error
+        'no-console': ['error', { allow: ['warn', 'error'] }],
+        camelcase: ['error', { properties: 'always' }],
+    },
+};
---
+++ PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,11 @@
+## Motivation
+
+<!-- Why are you making this change, what problem does it solve? -->
+
+## Changes
+
+<!-- What does this change exactly? Who will be affected? Include relevant screenshots, videos, links -->
+
+## Testing
+
+<!-- How can the reviewer confirm these changes do what you say they do? Are there automated tests? -->
